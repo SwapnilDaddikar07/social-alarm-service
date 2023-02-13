87e3108e7e47209a7f0be02943f30341db59ad9b
Swapnil Daddikar <swapnildaddikar07@gmail.com>
2023-02-13 23:59:52 +0530

    WIP COMMIT - Implemented DB helper. Used SQLx to initiate connection.

---
+++ dbHelper/db_helper.go
@@ -0,0 +1,10 @@
+package dbHelper
+
+import (
+	"github.com/jmoiron/sqlx"
+)
+
+// Connect TODO Read username , password , HOST from ENV variables
+func Connect() (*sqlx.DB, error) {
+	return sqlx.Connect("mysql", "alarm_user:alarm_pswd@localhost:3306/alarm_database")
+}
