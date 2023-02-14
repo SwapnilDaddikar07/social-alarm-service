f0b17340daca88a545b8c9883c37e6011e6c74dd
Dharmik Thakkar <dhth3498@colorado.edu>
2023-02-14 10:51:42 -0600

    Corrected connection string for mysql db connection

--- db_helper/db_helper.go
+++ db_helper/db_helper.go
@@ -2,11 +2,12 @@ package db_helper
 
 import (
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"os"
 )
 
 func Connect() (*sqlx.DB, error) {
-	dsn := fmt.Sprintf("%s:%s@%s:%s/%s", os.Getenv("DB_USER"), os.Getenv("DB_PWD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PWD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
 	return sqlx.Connect("mysql", dsn)
 }
