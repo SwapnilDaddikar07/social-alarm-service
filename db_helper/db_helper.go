af40fc58991fea3f789c6c50c04dba505b7f2618
Dharmik Thakkar <dhth3498@colorado.edu>
2023-02-14 11:52:34 -0600

    Change mysql connection string to include parse time

--- db_helper/db_helper.go
+++ db_helper/db_helper.go
@@ -8,6 +8,6 @@ import (
 )
 
 func Connect() (*sqlx.DB, error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", os.Getenv("DB_USER"), os.Getenv("DB_PWD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", os.Getenv("DB_USER"), os.Getenv("DB_PWD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
 	return sqlx.Connect("mysql", dsn)
 }
