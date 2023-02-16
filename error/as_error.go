ca21e1cf967628d9cda8c99c395c79107ccfb353
Swapnil Daddikar <swapnildaddikar07@gmail.com>
2023-02-16 22:02:28 +0530

    Introduced custom error model.

---
+++ error/as_error.go
@@ -0,0 +1,15 @@
+package error
+
+type ASError struct {
+	ErrorCode      string `json:"error_code"`
+	ErrorMessage   string `json:"error_message"`
+	HttpStatusCode int
+}
+
+func InternalServerError(errorMessage string) *ASError {
+	return &ASError{
+		ErrorCode:      "ERR_INTERNAL_SERVER_ERROR",
+		ErrorMessage:   errorMessage,
+		HttpStatusCode: 500,
+	}
+}
