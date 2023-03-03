e42ee621eb121674afbd3faf333e38c3fdda0da7
Swapnil Daddikar <swapnildaddikar07@gmail.com>
2023-03-03 22:17:27 +0530

    Introduced new custom errors.

--- error/as_error.go
+++ error/as_error.go
@@ -23,3 +23,27 @@ func BadRequestError(errorMessage string) *ASError {
 		HttpStatusCode: http.StatusBadRequest,
 	}
 }
+
+var AlarmIdMissing = &ASError{
+	ErrorCode:      "ERR_ALARM_ID_MISSING",
+	ErrorMessage:   "either repeating or non repeating alarm id should be provided",
+	HttpStatusCode: http.StatusBadRequest,
+}
+
+var InvalidUserIdError = &ASError{
+	ErrorCode:      "ERR_INVALID_USER_ID",
+	ErrorMessage:   "user id does not exist",
+	HttpStatusCode: http.StatusUnauthorized,
+}
+
+var DescriptionTooLongError = &ASError{
+	ErrorCode:      "ERR_DESCRIPTION_TOO_LONG",
+	ErrorMessage:   "description is too long.",
+	HttpStatusCode: http.StatusBadRequest,
+}
+
+var InvalidAlarmTypeError = &ASError{
+	ErrorCode:      "ERR_INVALID_ALARM_TYPE_ERROR",
+	ErrorMessage:   "alarm cannot be repeating as well as non-repeating",
+	HttpStatusCode: http.StatusBadRequest,
+}
