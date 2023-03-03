bab793539b68de70e2544c45e8d1a2d84620b816
Swapnil Daddikar <swapnildaddikar07@gmail.com>
2023-03-03 22:35:14 +0530

    WIP - Breaking commit.Method to create alarm in progress.

--- service/alarm_service.go
+++ service/alarm_service.go
@@ -4,12 +4,15 @@ import (
 	"github.com/gin-gonic/gin"
 	error2 "social-alarm-service/error"
 	"social-alarm-service/repository"
+	"social-alarm-service/request_model"
 	"social-alarm-service/response_model"
+	"time"
 )
 
 type AlarmService interface {
 	GetPublicNonExpiredAlarms(ctx *gin.Context, userId string) ([]response_model.EligibleAlarmsResponse, *error2.ASError)
 	GetMediaForAlarm(ctx *gin.Context, alarmId string) ([]response_model.MediaForAlarm, *error2.ASError)
+	CreateAlarm(ctx *gin.Context, request request_model.CreateAlarmRequest) (response_model.CreateAlarmResponse, *error2.ASError)
 }
 
 type alarmService struct {
@@ -38,4 +41,39 @@ func (as alarmService) GetMediaForAlarm(ctx *gin.Context, alarmId string) ([]res
 		return []response_model.MediaForAlarm{}, error2.InternalServerError("db fetch error when getting all media associated with given alarm id")
 	}
 	return response_model.MapToMediaForAlarmResponseList(alarmMedia), nil
+}
+
+func (as alarmService) CreateAlarm(ctx *gin.Context, request request_model.CreateAlarmRequest) (response_model.CreateAlarmResponse, *error2.ASError) {
+	if len(request.RepeatingSystemAlarmIds) == 0 && request.NonRepeatingSystemAlarmId == nil {
+		return response_model.CreateAlarmResponse{}, error2.AlarmIdMissing
+	}
+	if len(request.RepeatingSystemAlarmIds) >= 1 && (request.NonRepeatingSystemAlarmId != nil) {
+		return response_model.CreateAlarmResponse{}, error2.InvalidAlarmTypeError
+	}
+	//check the DB len
+	if len(request.Description) > 50 {
+		return response_model.CreateAlarmResponse{}, error2.DescriptionTooLongError
+	}
+	userExists, dbError := as.alarmRepository.UserExists(ctx, request.UserId)
+	if dbError != nil {
+		return response_model.CreateAlarmResponse{}, error2.InternalServerError("db fetch error")
+	}
+	if !userExists {
+		return response_model.CreateAlarmResponse{}, error2.InvalidUserIdError
+	}
+
+	if len(request.RepeatingSystemAlarmIds) > 0 {
+		saveRepeatingAlarm(ctx, request.RepeatingSystemAlarmIds, request.UserId, request.AlarmStartDateTime)
+	} else {
+		saveNonRepeatingAlarm(ctx, *request.NonRepeatingSystemAlarmId, request.UserId, request.AlarmStartDateTime)
+	}
+	panic("")
+}
+
+func saveNonRepeatingAlarm(ctx *gin.Context, systemAlarmId int, userId string, alarmStartDateTime time.Time) {
+
+}
+
+func saveRepeatingAlarm(ctx *gin.Context, repeatingSystemAlarmIds []int, userId string, alarmStartDateTime time.Time) {
+
 }
