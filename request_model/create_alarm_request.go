a7bb801792d74ec0a4038b33adeab89853e65168
Swapnil Daddikar <swapnildaddikar07@gmail.com>
2023-03-03 21:44:22 +0530

    Introduced binding required to create alarm model fields.

--- request_model/create_alarm_request.go
+++ request_model/create_alarm_request.go
@@ -1,10 +1,10 @@
 package request_model
 
 type CreateAlarmRequest struct {
-	AlarmStartDateTime        string `json:"alarm_start_date_time"`
+	AlarmStartDateTime        string `json:"alarm_start_date_time" binding:"required"`
 	Visibility                bool   `json:"visibility"`
 	Description               string `json:"description"`
-	UserId                    string `json:"user_id"`
+	UserId                    string `json:"user_id" binding:"required"` // change this after moving ID to token.
 	RepeatingSystemAlarmIds   []int  `json:"repeating_system_alarm_ids"`
 	NonRepeatingSystemAlarmId int    `json:"non_repeating_system_alarm_id"`
 }
