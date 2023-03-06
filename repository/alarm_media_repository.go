595b3c7fca6826992f9abd2272c815b39bccb1cd
Swapnil Daddikar <swapnildaddikar07@gmail.com>
2023-03-06 09:35:41 +0530

    Restructured query to get media for a given alarm id.

--- repository/alarm_media_repository.go
+++ repository/alarm_media_repository.go
@@ -21,7 +21,7 @@ func NewAlarmMediaRepository(db *sqlx.DB) AlarmMediaRepository {
 
 func (ar alarmMediaRepository) GetMediaForAlarm(ctx *gin.Context, alarmId string) ([]db_model.GetMediaForAlarm, error) {
 	mediaForAlarms := make([]db_model.GetMediaForAlarm, 0)
-	query := "select u.display_name , m.resource_url from alarm_media am  inner join media m on am.media_id = m.media_id  inner join users u on u.user_id = m.sender_id  where am.alarm_id = ? order by m.created_at desc"
+	query := "select u.display_name , m.resource_url from media m  inner join alarm_media am on m.media_id = am.media_id  inner join users u on u.user_id = m.sender_id  where am.alarm_id = ? order by m.created_at desc"
 
 	dbFetchError := ar.db.Select(&mediaForAlarms, query, alarmId)
 	if dbFetchError != nil {
