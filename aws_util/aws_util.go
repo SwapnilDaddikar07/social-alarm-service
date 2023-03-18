03b02fb337bc1ab28ef0b56a1211edab527f1e01
Swapnil Daddikar <swapnildaddikar07@gmail.com>
2023-03-18 20:07:00 +0530

    Introduced method to delete object from bucket.

--- aws_util/aws_util.go
+++ aws_util/aws_util.go
@@ -11,6 +11,7 @@ import (
 
 type AWSUtil interface {
 	UploadObject(ctx *gin.Context, file *multipart.File, bucketName string, key string) *error2.ASError
+	DeleteObject(ctx *gin.Context, bucketName string, key string) *error2.ASError
 }
 
 type awsUtil struct {
@@ -30,6 +31,18 @@ func (awsUtil awsUtil) UploadObject(ctx *gin.Context, file *multipart.File, buck
 	if err != nil {
 		fmt.Printf("Couldn't upload file to %v:%v. Here's why: %v\n", bucketName, key, err)
 		return error2.InternalServerError("aws upload object failed")
+	}
+	return nil
+}
+
+func (awsUtil awsUtil) DeleteObject(ctx *gin.Context, bucketName string, key string) *error2.ASError {
+	_, err := awsUtil.s3Client.DeleteObject(ctx, &s3.DeleteObjectInput{
+		Bucket: &bucketName,
+		Key:    &key,
+	})
+	if err != nil {
+		fmt.Printf("could not delete key %s from bucket %s \n", key, bucketName)
+		return error2.InternalServerError("could not delete object from s3")
 	}
 	return nil
 }
