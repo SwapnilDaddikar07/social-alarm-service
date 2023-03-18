2293d8283321a363356f37432fd14417bf75493b
Swapnil Daddikar <swapnildaddikar07@gmail.com>
2023-03-18 19:56:09 +0530

    Implemented upload media controller.

--- controller/alarm_media_controller.go
+++ controller/alarm_media_controller.go
@@ -1,14 +1,21 @@
 package controller
 
 import (
+	"bufio"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
+	"mime/multipart"
+	"net/http"
+	error2 "social-alarm-service/error"
 	"social-alarm-service/request_model"
 	"social-alarm-service/service"
+	"strings"
 )
 
 type AlarmMediaController interface {
 	GetMediaForAlarm(ctx *gin.Context)
+	UploadMedia(ctx *gin.Context)
 }
 
 type alarmMediaController struct {
@@ -24,7 +31,7 @@ func (amc alarmMediaController) GetMediaForAlarm(ctx *gin.Context) {
 
 	bindingErr := ctx.ShouldBindWith(&request, binding.JSON)
 	if bindingErr != nil {
-		ctx.AbortWithStatus(400)
+		ctx.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
 
@@ -35,4 +42,54 @@ func (amc alarmMediaController) GetMediaForAlarm(ctx *gin.Context) {
 	}
 
 	ctx.JSON(200, alarmMedia)
+}
+
+func (amc alarmMediaController) UploadMedia(ctx *gin.Context) {
+	ctx.Request.Body = http.MaxBytesReader(ctx.Writer, ctx.Request.Body, 14<<20)
+
+	alarmId := ctx.Request.FormValue("alarm_id")
+	if strings.TrimSpace(alarmId) == "" {
+		fmt.Println("alarm id is missing")
+		ctx.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	senderId := ctx.Request.FormValue("sender_id")
+	if strings.TrimSpace(senderId) == "" {
+		fmt.Println("sender id is missing")
+		ctx.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	file, _, err := ctx.Request.FormFile("media_file")
+	if err != nil {
+		fmt.Printf("error validating media file %v ", err)
+		ctx.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	defer file.Close()
+
+	if !isValidContentType(&file) {
+		fmt.Println("content type is invalid")
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, error2.ContentTypeNotSupported)
+		return
+	}
+
+	serviceError := amc.service.UploadMedia(ctx, alarmId, senderId, &file)
+	if serviceError != nil {
+		ctx.AbortWithStatusJSON(serviceError.HttpStatusCode, serviceError)
+		return
+	}
+
+	ctx.AbortWithStatus(http.StatusCreated)
+}
+
+func isValidContentType(file *multipart.File) bool {
+	buffIO := bufio.NewReader(*file)
+	sniffBytes, _ := buffIO.Peek(512)
+	contentType := http.DetectContentType(sniffBytes)
+
+	//TODO add relevant content types
+	return contentType != "video/mp4" || contentType != "audio/wave"
 }
