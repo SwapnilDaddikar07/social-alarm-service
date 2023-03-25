07b488372e34d4e6fa123116ad79aadff5b48e75
Swapnil Daddikar <swapnildaddikar07@gmail.com>
2023-03-26 00:15:46 +0530

    Introduced new constants for alarm status.

--- constants/constants.go
+++ constants/constants.go
@@ -2,5 +2,21 @@ package constants
 
 type AlarmVisibility string
 
-const AlarmPublicVisibility AlarmVisibility = "PUBLIC"
-const AlarmPrivateVisibility AlarmVisibility = "PRIVATE"
+const (
+	AlarmPublicVisibility  AlarmVisibility = "PUBLIC"
+	AlarmPrivateVisibility AlarmVisibility = "PRIVATE"
+)
+
+type AlarmStatus string
+
+const (
+	AlarmStatusON  AlarmStatus = "ON"
+	AlarmStatusOFF AlarmStatus = "OFF"
+)
+
+func GetAlarmStatus(status string) AlarmStatus {
+	if status == "ON" {
+		return AlarmStatusON
+	}
+	return AlarmStatusOFF
+}
