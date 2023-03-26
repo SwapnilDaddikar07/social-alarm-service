c90dbf196b96657e7a278508602600e02e8df273
Swapnil Daddikar <swapnildaddikar07@gmail.com>
2023-03-26 10:08:06 +0530

    Introduced missing non-repeating-device-alarm-id field in db struct for alarms.

--- db_model/alarms.go
+++ db_model/alarms.go
@@ -7,20 +7,21 @@ import (
 )
 
 type Alarms struct {
-	AlarmID            string                    `db:"alarm_id"`
-	UserID             string                    `db:"user_id"`
-	Visibility         constants.AlarmVisibility `db:"visibility"`
-	Description        string                    `db:"description"`
-	Status             string                    `db:"status"`
-	AlarmStartDateTime sql.NullTime              `db:"alarm_start_datetime"`
-	CreatedAt          sql.NullTime              `db:"created_at"`
-	MonDeviceAlarmId   int                       `db:"mon_device_alarm_id"`
-	TueDeviceAlarmId   int                       `db:"tue_device_alarm_id"`
-	WedDeviceAlarmId   int                       `db:"wed_device_alarm_id"`
-	ThuDeviceAlarmId   int                       `db:"thu_device_alarm_id"`
-	FriDeviceAlarmId   int                       `db:"fri_device_alarm_id"`
-	SatDeviceAlarmId   int                       `db:"sat_device_alarm_id"`
-	SunDeviceAlarmId   int                       `db:"sun_device_alarm_id"`
+	AlarmID                   string                    `db:"alarm_id"`
+	UserID                    string                    `db:"user_id"`
+	Visibility                constants.AlarmVisibility `db:"visibility"`
+	Description               string                    `db:"description"`
+	Status                    string                    `db:"status"`
+	AlarmStartDateTime        sql.NullTime              `db:"alarm_start_datetime"`
+	CreatedAt                 sql.NullTime              `db:"created_at"`
+	NonRepeatingDeviceAlarmId int                       `db:"device_alarm_id"`
+	MonDeviceAlarmId          int                       `db:"mon_device_alarm_id"`
+	TueDeviceAlarmId          int                       `db:"tue_device_alarm_id"`
+	WedDeviceAlarmId          int                       `db:"wed_device_alarm_id"`
+	ThuDeviceAlarmId          int                       `db:"thu_device_alarm_id"`
+	FriDeviceAlarmId          int                       `db:"fri_device_alarm_id"`
+	SatDeviceAlarmId          int                       `db:"sat_device_alarm_id"`
+	SunDeviceAlarmId          int                       `db:"sun_device_alarm_id"`
 }
 
 func (a Alarms) HasNonRepeatingAlarmExpired() bool {
