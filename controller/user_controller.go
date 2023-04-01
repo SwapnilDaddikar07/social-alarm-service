34281855169103ae8ad137089a60b57f9ab94025
Swapnil Daddikar <swapnildaddikar07@gmail.com>
2023-04-01 23:17:23 +0530

    Implemented controller for fetching profiles based on phone numbers

---
+++ controller/user_controller.go
@@ -0,0 +1,39 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"net/http"
+	"social-alarm-service/request_model"
+	"social-alarm-service/service"
+)
+
+type UserController interface {
+	GetProfiles(ctx *gin.Context)
+}
+
+type userController struct {
+	userService service.UserService
+}
+
+func NewUserController(userService service.UserService) UserController {
+	return userController{userService: userService}
+}
+
+func (uc userController) GetProfiles(ctx *gin.Context) {
+	request := request_model.GetProfilesRequest{}
+
+	bindingErr := ctx.ShouldBindWith(&request, binding.JSON)
+	if bindingErr != nil {
+		ctx.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+
+	userProfiles, serviceErr := uc.userService.GetProfiles(ctx, request.PhoneNumbers)
+	if serviceErr != nil {
+		ctx.AbortWithStatusJSON(serviceErr.HttpStatusCode, serviceErr)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, userProfiles)
+}
