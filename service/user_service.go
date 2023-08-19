a9af4ab3ee1b28e3c49b934de9cdc7f7d47fceca
Swapnil Daddikar <swapnil.daddikar@thoughtworks.com>
2023-08-19 22:28:21 +0530

    Moved logic to format mobile numbers to util.

--- service/user_service.go
+++ service/user_service.go
@@ -3,12 +3,10 @@ package service
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/nyaruka/phonenumbers"
 	error2 "social-alarm-service/error"
 	"social-alarm-service/repository"
 	"social-alarm-service/response_model"
 	"social-alarm-service/utils"
-	"strconv"
 )
 
 type UserService interface {
@@ -35,7 +33,7 @@ func (us userService) GetProfiles(ctx *gin.Context, userId string, phoneNumbers 
 		return response_model.GetProfilesResponse{}, error2.InternalServerError("unable to fetch profiles")
 	}
 
-	formattedNumbers := us.formatNumbers(regionCode, phoneNumbers)
+	formattedNumbers := utils.FormatNumbers(regionCode, phoneNumbers)
 
 	profiles, dbErr := us.userRepo.GetProfiles(ctx, formattedNumbers)
 	if dbErr != nil {
@@ -45,23 +43,4 @@ func (us userService) GetProfiles(ctx *gin.Context, userId string, phoneNumbers 
 
 	fmt.Printf("received %d number and retrived %d profiles", len(phoneNumbers), len(profiles))
 	return response_model.MapToProfilesResponse(profiles), nil
-}
-
-func (us userService) formatNumbers(defaultRegionCode string, nonformattedMobileNumbers []string) []string {
-	formattedMobileNumbers := make([]string, 0)
-
-	for _, mobileNum := range nonformattedMobileNumbers {
-		p, err := phonenumbers.Parse(mobileNum, defaultRegionCode)
-		if err != nil {
-			fmt.Println(fmt.Sprintf("could not parse the number %s ", mobileNum))
-			continue
-		}
-
-		formattedNumber := fmt.Sprintf("%s%s%s", "+", fmt.Sprint(p.GetCountryCode()), strconv.FormatUint(p.GetNationalNumber(), 10))
-		fmt.Println(fmt.Sprintf("unformatted number is %s and formatted number is %s", mobileNum, formattedNumber))
-
-		formattedMobileNumbers = append(formattedMobileNumbers, formattedNumber)
-	}
-
-	return formattedMobileNumbers
 }
--- utils/mobile_number_util.go
+++ utils/mobile_number_util.go
@@ -1,8 +1,10 @@
 package utils
 
 import (
+	"fmt"
 	"github.com/nyaruka/phonenumbers"
 	error2 "social-alarm-service/error"
+	"strconv"
 )
 
 func GetRegionCode(mobileNumber string) (string, *error2.ASError) {
@@ -12,4 +14,23 @@ func GetRegionCode(mobileNumber string) (string, *error2.ASError) {
 	}
 
 	return phonenumbers.GetRegionCodeForCountryCode(int(p.GetCountryCode())), nil
+}
+
+func FormatNumbers(defaultRegionCode string, nonformattedMobileNumbers []string) []string {
+	formattedMobileNumbers := make([]string, 0)
+
+	for _, mobileNum := range nonformattedMobileNumbers {
+		p, err := phonenumbers.Parse(mobileNum, defaultRegionCode)
+		if err != nil {
+			fmt.Println(fmt.Sprintf("could not parse the number %s ", mobileNum))
+			continue
+		}
+
+		formattedNumber := fmt.Sprintf("%s%s%s", "+", fmt.Sprint(p.GetCountryCode()), strconv.FormatUint(p.GetNationalNumber(), 10))
+		fmt.Println(fmt.Sprintf("unformatted number is %s and formatted number is %s", mobileNum, formattedNumber))
+
+		formattedMobileNumbers = append(formattedMobileNumbers, formattedNumber)
+	}
+
+	return formattedMobileNumbers
 }
