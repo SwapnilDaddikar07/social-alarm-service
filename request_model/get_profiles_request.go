d2199c957662d996356d81521c6de2799962cfc9
Swapnil Daddikar <swapnil.daddikar@thoughtworks.com>
2023-08-19 19:52:30 +0530

    Temporarily introduced user id in get profiles request model.

--- request_model/get_profiles_request.go
+++ request_model/get_profiles_request.go
@@ -1,5 +1,6 @@
 package request_model
 
 type GetProfilesRequest struct {
+	UserId       string   `json:"user_id" binding:"required"`
 	PhoneNumbers []string `json:"phone_numbers" binding:"required"`
 }
