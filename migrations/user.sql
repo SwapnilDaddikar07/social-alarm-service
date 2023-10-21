d48b7b8425fbaeb6fdd18d43662482487081dbb4
Swapnil Daddikar <swapnil.daddikar@thoughtworks.com>
2023-10-21 20:50:29 +0530

    Introduced current-timezone column in users table

--- migrations/user.sql
+++ migrations/user.sql
@@ -1,7 +1,8 @@
 CREATE TABLE `users`
 (
-    `user_id`      varchar(50) NOT NULL,
-    `display_name` varchar(100) DEFAULT NULL,
-    `phone_number` varchar(45)  DEFAULT NULL,
+    `user_id`          varchar(50)  NOT NULL,
+    `display_name`     varchar(100) DEFAULT NULL,
+    `phone_number`     varchar(45)  DEFAULT NULL,
+    `current_timezone` varchar(200) NOT NULL,
     PRIMARY KEY (`user_id`)
 );
