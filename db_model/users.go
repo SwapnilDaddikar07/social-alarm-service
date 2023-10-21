dd9d54a0d665c78420945ce8779da49794063b1d
Swapnil Daddikar <swapnil.daddikar@thoughtworks.com>
2023-10-21 20:53:36 +0530

    Introduced current-timezone field in db-model struct representing users table

--- db_model/users.go
+++ db_model/users.go
@@ -1,7 +1,8 @@
 package db_model
 
 type User struct {
-	UserId      string `db:"user_id"`
-	PhoneNumber string `db:"phone_number"`
-	DisplayName string `db:"display_name"`
+	UserId          string `db:"user_id"`
+	PhoneNumber     string `db:"phone_number"`
+	DisplayName     string `db:"display_name"`
+	CurrentTimezone string `db:"current_timezone"`
 }
